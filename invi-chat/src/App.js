87d62b5ee4a858fea30b0a9496128a63961b75e7
Antonio Monroy <tony_monroe1212@hotmail.com>
2021-09-21 17:06:27 -0500

    Added react routing and first components

--- invi-chat/src/App.js
+++ invi-chat/src/App.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react' 
+import Test from './components/Test';
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"; 
+import Aside from './components/Aside';
+
 
 function App() {
   return (
-    <div className="App">
+    <> 
+      <Router> 
+      <div className="App">
 
-    </div>
+        <div className="window-container"> 
+          
+          <Aside/>
+          
+          <div> 
+            <nav> 
+              <ul> 
+                <Link to="/"> Inicio </Link>
+              </ul>
+              <ul> 
+                <Link to="/Test"> Test </Link>
+              </ul>
+              <ul> 
+                <Link to="/Dashboard"> Dashboard </Link>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        
+      </div> 
+
+        <Switch> 
+          <Route path="/Test" exact component={()=> <Test/> } > 
+            <Test/>
+          </Route>
+        </Switch>
+
+      </Router>
+    </>
+    
   );
 }
 
+
+ 
 export default App;
