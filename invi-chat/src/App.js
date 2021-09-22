23fe2c6218d1eef3866f84d4e9c4d72dd1e16e5a
Antonio Monroy <tony_monroe1212@hotmail.com>
2021-09-22 18:04:38 -0500

    Installed firebase SDK and rect-firebase-hooks

--- invi-chat/src/App.js
+++ invi-chat/src/App.js
@@ -3,11 +3,35 @@ import Test from './components/Test';
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"; 
 import Aside from './components/Aside';
 import Dashboard from './components/Dashboard';
+import firebase from 'firebase'; 
+import auth from 'firebase/auth'; 
+import { useAuthState } from 'react-firebase-hooks/auth';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCyddp2vs8JJfytLxDy5t3tQswv1TT1WwE",
+  authDomain: "invi-chat.firebaseapp.com",
+  projectId: "invi-chat",
+  storageBucket: "invi-chat.appspot.com",
+  messagingSenderId: "758776663456",
+  appId: "1:758776663456:web:1a438e2a7cc61eae1728c5",
+  measurementId: "G-55NNVS23TK"
+};
 
 function App() {
+  
+  // firebase.initializeApp(firebaseConfig);
+  
+  let user = true; 
+
+  if(!user) return ( 
+    <div> 
+        <h1> Login my gee! </h1>
+    </div>
+  )
+
   return (
     <> 
+
       <Router> 
       <div className="App">
 
